c9d718e28efa8e6c9f439ab1030e402c20fe3e43
shiranon <love.frame.craft@gmail.com>
2025-03-23 18:20:42 +0900

    fix: rubocop

--- backend/app/serializers/api/v1/mylist_serializer.rb
+++ backend/app/serializers/api/v1/mylist_serializer.rb
@@ -14,30 +14,44 @@ module Api
       end
 
       def quizzes
-        object.quizzes.map do |quiz|
-          {
-            id: quiz.id,
-            question: quiz.question,
-            answer: quiz.answer,
-            description: quiz.description,
-            quiz_image_url: quiz_image_url(quiz),
-            comic: {
-              id: quiz.comic.id,
-              title: quiz.comic.title,
-              author: {
-                id: quiz.comic.author_id,
-                name: quiz.comic.author&.name || '不明'
-              },
-              magazine: {
-                id: quiz.comic.magazine_id,
-                name: quiz.comic.magazine&.name || '不明'
-              }
-            }
-          }
-        end
+        object.quizzes.map { |quiz| serialize_quiz(quiz) }
       end
 
       private
+
+      def serialize_quiz(quiz)
+        {
+          id: quiz.id,
+          question: quiz.question,
+          answer: quiz.answer,
+          description: quiz.description,
+          quiz_image_url: quiz_image_url(quiz),
+          comic: serialize_comic(quiz.comic)
+        }
+      end
+
+      def serialize_comic(comic)
+        {
+          id: comic.id,
+          title: comic.title,
+          author: serialize_author(comic),
+          magazine: serialize_magazine(comic)
+        }
+      end
+
+      def serialize_author(comic)
+        {
+          id: comic.author_id,
+          name: comic.author&.name || '不明'
+        }
+      end
+
+      def serialize_magazine(comic)
+        {
+          id: comic.magazine_id,
+          name: comic.magazine&.name || '不明'
+        }
+      end
 
       def quiz_image_url(quiz)
         return nil unless quiz.quiz_image.attached?
